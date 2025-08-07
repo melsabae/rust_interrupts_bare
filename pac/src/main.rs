7eb0c424b8fabe8b679540973ee63bf862cfdd52
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2025-08-07 19:25:58 -0400

    remove device.x from pac version, work on getting the usart to run, add the board crate as the logical next step up from the pac

--- pac/src/main.rs
+++ pac/src/main.rs
@@ -5,460 +5,94 @@
 // use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 // use panic_abort as _; // requires nightly
 // use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
-use core::arch::asm;
-use core::ptr;
-//use core::sync::atomic::{AtomicUsize, Ordering};
+use core::cell::RefCell;
+use cortex_m::{asm, peripheral::NVIC};
 use cortex_m_rt::entry;
+use cortex_m_semihosting::hprintln;
+use critical_section::Mutex;
+use stm32f3xx_hal::gpio;
+use stm32f3xx_hal::gpio::{Edge, GpioExt, Input};
+use stm32f3xx_hal::interrupt;
+use stm32f3xx_hal::pac;
+use stm32f3xx_hal::prelude::_embedded_hal_digital_ToggleableOutputPin;
+use stm32f3xx_hal::prelude::_stm32f3xx_hal_rcc_RccExt;
+use stm32f3xx_hal::prelude::_stm32f3xx_hal_syscfg_SysCfgExt;
 
-//static COUNTER: AtomicUsize = AtomicUsize::new(0);
-const USART1_BASE: *mut u32 = 0x4001_3800 as *mut u32;
-
-unsafe extern "C" {
-    fn DefaultHandler();
-    fn EXTI0_Handler();
-}
-
-#[unsafe(no_mangle)]
-fn exti0_handler() {
-    let exti_base = 0x4001_0400 as *mut u32;
-    let exti_pr1 = exti_base.wrapping_byte_add(0x14);
-    let exti0_pr1_clear = 0b1_u32; // exti0 source interrupt
-    let usart1_tdr = USART1_BASE.wrapping_byte_add(0x28) as *mut u8;
-
-    unsafe {
-        // clear exti0 interrupt pending
-        ptr::write_volatile(exti_pr1, exti0_pr1_clear);
-
-        // send a byte over usart
-        ptr::write_volatile(usart1_tdr, 0x24_u8); // '$'
-    }
-}
+static BUTTON: Mutex<RefCell<Option<gpio::PA0<Input>>>> = Mutex::new(RefCell::new(None));
+//static USART: Mutex<RefCell<Option<usart1::TDR>>> = Mutex::new(RefCell::new(None));
 
 #[entry]
 fn main() -> ! {
-    // set pointers
-    let rcc_base = 0x4002_1000 as *mut u32;
-    let gpio_a_base = 0x4800_0000 as *mut u32;
-    let syscfg_base = 0x4001_0000 as *mut u32;
-    let exti_base = 0x4001_0400 as *mut u32;
-    //let nvic_base = 0xe000_e100 as *mut u32;
-    let nvic_base = 0xe000_e000 as *mut u32;
-    let nvic_iser0 = nvic_base.wrapping_byte_add(0x100);
-    let rcc_ahbenr = rcc_base.wrapping_byte_add(0x14);
-    let rcc_apb2enr = rcc_base.wrapping_byte_add(0x18);
-    let gpio_a_moder = gpio_a_base.wrapping_byte_add(0x00);
-    let gpio_a_pupdr = gpio_a_base.wrapping_byte_add(0x0c);
-    let gpio_a_afrh = gpio_a_base.wrapping_byte_add(0x24);
-    let syscfg_exticr1 = syscfg_base.wrapping_byte_add(0x08);
-    let exti_imr1 = exti_base.wrapping_byte_add(0x00);
-    let exti_rtsr1 = exti_base.wrapping_byte_add(0x08);
-    let usart1_cr1 = USART1_BASE.wrapping_byte_add(0x00);
-    //let usart1_cr2 = USART1_BASE.wrapping_byte_add(0x04);
-    let usart1_brr = USART1_BASE.wrapping_byte_add(0x0C);
+    let peripherals = pac::Peripherals::take().unwrap();
 
-    // name the values
-    let rcc_gpio_a_en_bit = (1 << 17) as u32;
-    let rcc_syscfg_en_bit = 0b1_u32;
-    let gpio_a_0_mode_mask = 0b11_u32;
-    let gpio_a_0_mode = 0b00_u32;
-    let gpio_a_0_pupdr_mask = 0b11_u32;
-    let gpio_a_0_pupd = 0b10_u32;
-    let syscfg_exticr1_exti0_mask = 0b1111_u32;
-    let syscfg_exticr1_exti0_pa0 = 0b000_u32; // bit 3 is reserved, lowest 3 bits is meaningful
-    let exti_imr1_pa0_bit = 0b1_u32;
-    let exti_rtsr1_pa0_bit = 0b1_u32;
-    let nvic_iser0_exti0_mask = 1 << 6; // exti0 interrupt is interrupt 6
+    // enable PA10 and PA9 as alternate function 7, USART RX/TX respectively
+    peripherals.GPIOA.moder.modify(|_, w| w.moder9().bits(0b10).moder10().bits(0b10));
 
-    // known, fixed values, lets us check our work with byte oriented registers
-    //let _b1 = unsafe { ptr::read_volatile(0xe000_ed00 as *mut u8) };
-    //let _b2 = unsafe { ptr::read_volatile(0xe000_ed01 as *mut u8) };
-    //let _b3 = unsafe { ptr::read_volatile(0xe000_ed02 as *mut u8) };
-    //let _b4 = unsafe { ptr::read_volatile(0xe000_ed03 as *mut u8) };
+    // set pa9 as an output
+    peripherals.GPIOA.otyper.modify(|_, w| w.ot9().bit(true));
 
-    // set up IO PA0 interrupt, connected to the USER button on the board
-    unsafe {
-        asm!("cpsie i"); // enable interrupts
+    // set pull downs for PA10
+    unsafe { peripherals.GPIOA.pupdr.modify(|_, w| w.pupdr10().bits(0b10).pupdr9().bits(0b00)) };
 
-        // enable EXTI0 interrupt in NVIC
-        ptr::write_volatile(
-            nvic_iser0,
-            ptr::read_volatile(nvic_iser0) | nvic_iser0_exti0_mask,
-        );
+    // set alternate functions for PA9/PA10 for USART TX/RX
+    peripherals.GPIOA.afrh.modify(|_, w| w.afrh10().bits(0b0111).afrh9().bits(0b0111));
 
-        // enable GPIOA clock
-        ptr::write_volatile(
-            rcc_ahbenr,
-            ptr::read_volatile(rcc_ahbenr) | rcc_gpio_a_en_bit,
-        );
+    // enable USART1 peripheral clock
+    peripherals.RCC.apb2enr.modify(|_, w| w.usart1en().bit(true));
 
-        // enable SYSCFG clock
-        ptr::write_volatile(
-            rcc_apb2enr,
-            ptr::read_volatile(rcc_apb2enr) | rcc_syscfg_en_bit,
-        );
+    // set baud rate for 8MHz default clock
+    peripherals.USART1.brr.modify(|_, w| w.brr().bits((8_000_000 / 115_200) as u16));
 
-        // set PA0 to input
-        ptr::write_volatile(
-            gpio_a_moder,
-            (ptr::read_volatile(gpio_a_moder) & !gpio_a_0_mode_mask) | gpio_a_0_mode,
-        );
+    // transmitter enable, receiver enable
+    peripherals.USART1.cr1.modify(|_, w| w.te().bit(true).re().bit(true));
 
-        // set PA0 pull-downs
-        ptr::write_volatile(
-            gpio_a_pupdr,
-            (ptr::read_volatile(gpio_a_pupdr) & !gpio_a_0_pupdr_mask) | gpio_a_0_pupd,
-        );
+    // auto baud rate enable
+    //peripherals.USART1.cr2.modify(|_, w| w.abren().bit(true));
 
-        // set EXTI0 input to PA0
-        ptr::write_volatile(
-            syscfg_exticr1,
-            (ptr::read_volatile(syscfg_exticr1) & !syscfg_exticr1_exti0_mask)
-                | syscfg_exticr1_exti0_pa0,
-        );
+    let mut rcc = peripherals.RCC.constrain();
+    let mut exti = peripherals.EXTI;
+    let mut syscfg = peripherals.SYSCFG.constrain(&mut rcc.apb2);
+    let mut gpioa = peripherals.GPIOA.split(&mut rcc.ahb);
+    let mut gpioe = peripherals.GPIOE.split(&mut rcc.ahb);
 
-        // enable EXTI0 interrupt
-        ptr::write_volatile(exti_imr1, ptr::read_volatile(exti_imr1) | exti_imr1_pa0_bit);
+    let mut led = gpioe
+        .pe8
+        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
 
-        // enable rising edge interrupt for EXTI0
-        ptr::write_volatile(
-            exti_rtsr1,
-            ptr::read_volatile(exti_rtsr1) | exti_rtsr1_pa0_bit,
-        );
-    }
+    let mut button = gpioa
+        .pa0
+        .into_pull_down_input(&mut gpioa.moder, &mut gpioa.pupdr);
 
-    // set up USART1
-    unsafe {
-        // alternate function mode
-        let gpio_a_9_10_moder_mask = 0b11_11_11_11_11_00_00_11_11_11_11_11_11_11_11_11_u32;
-        let gpio_a_9_10_moder_mode = 0b00_00_00_00_00_10_10_00_00_00_00_00_00_00_00_00_u32;
+    syscfg.select_exti_interrupt_source(&button);
+    button.trigger_on_edge(&mut exti, Edge::Rising);
+    button.enable_interrupt(&mut exti);
 
-        // pull down on RX line/PA10
-        let gpio_a_9_pupdr_mask = 0b11_11_11_11_11_00_11_11_11_11_11_11_11_11_11_11_u32;
-        let gpio_a_9_pupdr = 0b00_00_00_00_00_10_00_00_00_00_00_00_00_00_00_00_u32;
+    unsafe { NVIC::unmask(button.interrupt()) }
 
-        let gpio_a_9_10_afrh_mask = 0b1111_1111_1111_1111_1111_0000_0000_1111_u32;
-        let gpio_a_9_10_afrh = 0b0000_0000_0000_0000_0000_0000_0111_0111_u32;
+    critical_section::with(|cs| *BUTTON.borrow(cs).borrow_mut() = Some(button));
+    //critical_section::with(|cs| *USART.borrow(cs).borrow_mut() = Some(usart1.tdr));
 
-        ptr::write_volatile(
-            gpio_a_moder,
-            (ptr::read_volatile(gpio_a_moder) & gpio_a_9_10_moder_mask) | gpio_a_9_10_moder_mode,
-        );
+    loop {
+        asm::wfi();
 
-        // set pull ups/pulldowns for USART1
-        ptr::write_volatile(
-            gpio_a_pupdr,
-            (ptr::read_volatile(gpio_a_pupdr) & gpio_a_9_pupdr_mask) | gpio_a_9_pupdr,
-        );
+        peripherals.USART1.tdr.modify(|_, w| w.tdr().bits(0x24));
+        led.toggle().unwrap();
 
-        // set alternate function modes to 7, USART1 RX/TX
-        ptr::write_volatile(
-            gpio_a_afrh,
-            (ptr::read_volatile(gpio_a_afrh) & gpio_a_9_10_afrh_mask) | gpio_a_9_10_afrh,
-        );
-
-        // enable clock to USART1
-        ptr::write_volatile(
-            rcc_apb2enr,
-            ptr::read_volatile(rcc_apb2enr) | ((1 << 14) as u32),
-        );
-
-        // TX/RX/USART enable
-        ptr::write_volatile(usart1_cr1, ptr::read_volatile(usart1_cr1) | 0b1101_u32);
-
-        // 115200 baud, assuming 8 MHz HSI clock is being used
-        ptr::write_volatile(usart1_brr, (8_000_000 / 115200) as u32);
-
-        //// auto baud rate enable
-        //ptr::write_volatile(
-        //    usart1_cr2,
-        //    ptr::read_volatile(usart1_cr2) | ((1 << 20) as u32),
-        //);
-    }
-
-    unsafe {
-        loop {
-            asm!("nop");
-            //asm!("wfi"); // wait for interrupt
-            //COUNTER.store(COUNTER.load(Ordering::Relaxed) + 1, Ordering::Relaxed);
-
-            // TODO: loop should write a repeating sequence of characters to USART1, verify in minicom
-        }
+        hprintln!("awoken");
     }
 }
 
-#[panic_handler]
-fn panic_handler(_ctx: &core::panic::PanicInfo) -> ! {
-    loop {}
+#[interrupt]
+fn EXTI0() {
+    critical_section::with(|cs| {
+        BUTTON
+            .borrow(cs)
+            .borrow_mut()
+            .as_mut()
+            .unwrap()
+            .clear_interrupt();
+    });
 }
 
-#[repr(C)]
-pub union NVIC_Vector {
-    handler: unsafe extern "C" fn(),
-    reserved: u32,
-}
-
-// this interrupts table is placed at an offset of 0x40 within the NVIC_Vector table
-// 0x40 == 64 == 16 * 4, this starts at index 16, which is this Cortex M4's defined IRQn 0
-// 4 byte of alignment on a 32 bit mcu
-// this is done because the first 16 vectors are reserved for the cortex
-#[unsafe(link_section = ".vector_table.interrupts")]
-#[unsafe(no_mangle)]
-pub static __INTERRUPTS: [NVIC_Vector; 85] = [
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: EXTI0_Handler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-    NVIC_Vector {
-        handler: DefaultHandler,
-    },
-];
