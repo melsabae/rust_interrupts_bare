653a3f82a0a855fbf369e9cd7c5dc95105df384b
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2025-08-13 17:02:06 -0400

    i dont like this HAL

--- embassy/src/main.rs
+++ embassy/src/main.rs
@@ -7,125 +7,45 @@
 // use panic_itm as _; // logs messages over ITM; requires ITM support
 use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
-use embassy_stm32;
-use embassy_time::Timer;
-use cortex_m_semihosting::hprintln;
+use core::default;
+use embassy_stm32::{
+    exti::ExtiInput,
+    gpio::{Level, Output, Pull, Speed},
+    mode::Blocking,
+    usart,
+    usart::Uart,
+};
 
 #[embassy_executor::task]
-async fn chatter() {
+async fn buttoner(
+    mut button: ExtiInput<'static>,
+    mut led: Output<'static>,
+    mut usart: Uart<'static, Blocking>,
+) {
     loop {
-        hprintln!("ahoy");
-        Timer::after_millis(500).await;
-        hprintln!("yoha");
-        Timer::after_millis(500).await;
+        button.wait_for_rising_edge().await;
+        led.toggle();
+        usart.blocking_write(&[0x24]).unwrap();
     }
 }
 
 #[embassy_executor::main]
 async fn main(spawner: embassy_executor::Spawner) {
-    let _p = embassy_stm32::init(Default::default());
+    let peripherals = embassy_stm32::init(Default::default());
 
-    spawner.spawn(chatter()).unwrap();
+    let button = ExtiInput::new(peripherals.PA0, peripherals.EXTI0, Pull::Down);
+    let led = Output::new(peripherals.PE8, Level::Low, Speed::Low);
+    let mut usart_config = <usart::Config as default::Default>::default();
 
-    //let peripherals = Peripherals::take().unwrap();
+    usart_config.baudrate = 115200;
 
-    //// enable GPIOA/GPIOE peripheral clocks
-    //peripherals
-    //    .RCC
-    //    .ahbenr()
-    //    .modify(|_, w| w.iopaen().bit(true).iopeen().bit(true));
+    let usart = Uart::new_blocking(
+        peripherals.USART1,
+        peripherals.PA10,
+        peripherals.PA9,
+        usart_config,
+    )
+    .unwrap();
 
-    //// enable USART1 peripheral clock
-    //peripherals
-    //    .RCC
-    //    .apb2enr()
-    //    .modify(|_, w| w.usart1en().bit(true));
-
-    //// set PA9/10 to alternate function mode
-    //// set PA0 to input
-    //peripherals.GPIOA.moder().modify(unsafe {
-    //    |_, w| {
-    //        w.moder10()
-    //            .bits(0b10)
-    //            .moder9()
-    //            .bits(0b10)
-    //            .moder0()
-    //            .bits(0b00)
-    //    }
-    //});
-
-    //// set pa9 as a push-pull output
-    //peripherals.GPIOA.otyper().modify(|_, w| w.ot9().bit(false));
-
-    //// set pull downs for PA10, disable for PA9, pull down PA0
-    //unsafe {
-    //    peripherals.GPIOA.pupdr().modify(|_, w| {
-    //        w.pupdr10()
-    //            .bits(0b10)
-    //            .pupdr9()
-    //            .bits(0b00)
-    //            .pupdr0()
-    //            .bits(0b10)
-    //    })
-    //};
-
-    //// set alternate functions for PA9/PA10 to USART TX/RX
-    //peripherals
-    //    .GPIOA
-    //    .afrh()
-    //    .modify(unsafe { |_, w| w.afrh10().bits(0b0111).afrh9().bits(0b0111) });
-
-    //// set PA0 function to GPIO
-    //peripherals
-    //    .GPIOA
-    //    .afrl()
-    //    .modify(unsafe { |_, w| w.afrl0().bits(0b0000) });
-
-    //// set USART1 baud rate for 8MHz default clock to 115200
-    //peripherals
-    //    .USART1
-    //    .brr()
-    //    .modify(unsafe { |_, w| w.brr().bits((8_000_000 / 115_200) as u16) });
-
-    //// transmitter enable, receiver enable, usart enable
-    //peripherals
-    //    .USART1
-    //    .cr1()
-    //    .modify(|_, w| w.te().bit(true).re().bit(true).ue().bit(true));
-
-    //// set exti0 interrupt source to PA0
-    //peripherals
-    //    .SYSCFG
-    //    .exticr1()
-    //    .modify(unsafe { |_, w| w.exti0().bits(0b0000) });
-
-    //// enable interrupts, events
-    //peripherals.EXTI.imr1().modify(|_, w| w.mr0().bit(true));
-    //peripherals.EXTI.emr1().modify(|_, w| w.mr0().bit(true));
-
-    //// enable rising edge interrupt for exti0
-    //peripherals.EXTI.rtsr1().modify(|_, w| w.tr0().bit(true));
-
-    //// auto baud rate enable
-    ////peripherals.USART1.cr2.modify(|_, w| w.abren().bit(true));
-
-    ////let mut gpioe = peripherals.GPIOE.split(&mut rcc.ahb);
-
-    ////let mut led = gpioe
-    ////    .pe8
-    ////    .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-
-    //unsafe { NVIC::unmask(interrupt::EXTI0) }
-
-    //critical_section::with(|cs| {
-    //    *USART.borrow(cs).borrow_mut() = Some(peripherals.USART1);
-    //    *EXTI.borrow(cs).borrow_mut() = Some(peripherals.EXTI);
-    //});
-
-    //loop {
-    //    asm::wfi();
-
-    //    hprintln!("awoken");
-    //}
+    spawner.spawn(buttoner(button, led, usart)).unwrap();
 }
-
